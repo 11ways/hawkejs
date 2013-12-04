d15da4cf6f7df3d2068d0f91cadd4fdcda3699ad
Jelle De Loecker <jelle@codedor.be>
2013-12-04 15:51:48 +0200

    Fix ClientSide variable inside payload, add jsonify script and expose vendor folder

---
+++ vendor/jsonify.js
@@ -0,0 +1,20 @@
+(function($) {
+$.fn.jsonify = function(options) {
+	var settings = $.extend({
+		stringify : false
+	}, options);
+	var json = {};
+	$.each(this.serializeArray(), function() {
+		if (json[this.name]) {
+			if (!json[this.name].push)
+				json[this.name] = [json[this.name]];
+			json[this.name].push(this.value || '');
+		} else
+			json[this.name] = this.value || '';
+	});
+	if(settings.stringify)
+		return JSON.stringify(json);
+	else
+		return json;
+};
+})(jQuery);
