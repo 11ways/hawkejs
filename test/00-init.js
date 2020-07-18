cf6744c912ac69e8ab1bb15ea732001e2d62c76f
Jelle De Loecker <jelle@elevenways.be>
2020-07-18 23:28:39 +0200

    Move unit testing browser init code to the 00-init file

--- test/00-init.js
+++ test/00-init.js
@@ -1,7 +1,199 @@
-var assert   = require('assert'),
-    Hawkejs  = require('../index.js'),
-    fs       = require('fs'),
+let puppeteer = require('puppeteer'),
+    assert    = require('assert'),
+    Hawkejs   = require('../index.js'),
+    http      = require('http'),
+    fs        = require('fs'),
     hawkejs;
+
+let navigations = 0,
+    do_coverage = !!global.__coverage__,
+    coverage,
+    browser,
+    page;
+
+global.do_coverage = do_coverage;
+
+global.fetchCoverage = async function fetchCoverage() {
+
+	if (!page) {
+		return;
+	}
+
+	let temp = await page.evaluate(function getCoverage() {
+		return window.__coverage__;
+	});
+
+	if (temp) {
+		coverage = temp;
+	}
+
+	return coverage;
+};
+
+global.setLocation = async function setLocation(path) {
+
+	let url;
+
+	if (!page) {
+		await loadBrowser();
+	}
+
+	if (navigations && do_coverage) {
+		await fetchCoverage();
+	}
+
+	navigations++;
+
+	if (path.indexOf('http') == -1) {
+		url = 'http://127.0.0.1:' + port + path;
+	} else {
+		url = path;
+	}
+
+	await page.goto(url);
+
+	if (coverage) {
+		await page.evaluate(function setCoverage(coverage) {
+			window.__coverage__ = coverage;
+		}, coverage);
+	}
+};
+
+global.evalPage = function evalPage(fnc) {
+	return page.evaluate(fnc);
+};
+
+global.despace = function despace(text) {
+	return text.trim().replace(/\n/g, ' ').replace(/\s\s+/g, ' ');
+};
+
+async function loadBrowser() {
+	browser = await puppeteer.launch();
+	page = await browser.newPage();
+	global.browser = browser;
+
+	page.on('console', function(msg) {
+		var pieces = ['[BROWSER]'],
+		    args = msg.args(),
+		    args;
+
+		for (arg of args) {
+			let remote = arg._remoteObject;
+
+			if (remote.type == 'string') {
+				pieces.push(remote.value);
+			} else if (remote.subtype == 'node') {
+				pieces.push('\x1b[1m\x1b[36m<' + remote.description + '>\x1b[0m');
+				//console.log(remote.preview);
+			} else if (remote.className) {
+				pieces.push('\x1b[1m\x1b[33m{' + remote.type + ' ' + remote.className + '}\x1b[0m');
+			} else if (remote.value != null) {
+				pieces.push(remote.value);
+			} else {
+				pieces.push(remote);
+			}
+		}
+
+		console.log(...pieces);
+	});
+
+	hawkejs = new Hawkejs();
+	hawkejs.addViewDirectory(__dirname + '/templates');
+
+	await new Promise(function(resolve, reject) {
+		server = http.createServer(function onReq(req, res) {
+
+			var url = __Protoblast.Classes.RURL.parse(req.url),
+			    renderer = hawkejs.createRenderer(),
+			    templates;
+
+			req.renderer = renderer;
+			renderer.internal('url', url);
+
+			if (url.pathname == '/hawkejs/hawkejs-client.js') {
+
+				__Protoblast.getClientPath({
+					modify_prototypes : true,
+					ua                : req.headers.useragent,
+					enable_coverage   : !!global.__coverage__,
+				}).done(function gotClientFile(err, path) {
+
+					if (err) {
+						throw err;
+					}
+
+					res.writeHead(200, {'Content-Type': 'application/javascript'});
+
+					fs.createReadStream(path).pipe(res);
+				});
+
+				return;
+			}
+
+			// Serve multiple template files
+			if (url.pathname == '/hawkejs/templates') {
+
+				var names = url.param('name');
+
+				if (!names) {
+					res.status = 500;
+					return res.end('No template names have been given');
+				}
+
+				hawkejs.getFirstAvailableSource(names, function gotResult(err, result) {
+
+					if (err) {
+						res.status = 500;
+						return res.end(String(err));
+					}
+
+					if (!result || !result.name) {
+						res.status = 400;
+						return res.end('Could not find any of the given templates');
+					}
+
+					res.setHeader('content-type', 'application/json;charset=utf-8');
+					res.end(JSON.stringify(result));
+				});
+
+				return;
+			}
+
+			templates = url.pathname.slice(1);
+
+			renderer.renderHTML(templates).done(function afterRender(err, html) {
+
+				var mimetype;
+
+				if (err != null) {
+					throw err;
+				}
+
+				if (typeof html !== 'string') {
+
+					// Stringify using json-dry
+					html = __Protoblast.Bound.JSON.dry(html);
+
+					// Tell the client to expect a json-dry response
+					mimetype = 'application/json-dry';
+				} else {
+					mimetype = 'text/html';
+				}
+
+				// Only send the mimetype if it hasn't been set yet
+				if (res.getHeader('content-type') == null) {
+					res.setHeader('content-type', mimetype + ";charset=utf-8");
+				}
+
+				res.end(html);
+			});
+
+		}).listen(0, '0.0.0.0', function listening() {
+			port = server.address().port;
+			resolve();
+		});
+	});
+}
 
 describe('Hawkejs', function() {
 
