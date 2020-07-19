8d3796d452a282b44baac7562505675b5f48e00f
Jelle De Loecker <jelle@elevenways.be>
2020-07-19 15:14:05 +0200

    fix: DOM spotting sometimes called back twice for the same element

--- test/80-dom_spotting.js
+++ test/80-dom_spotting.js
@@ -40,6 +40,9 @@ describe('DOM Spotting', function() {
 				{ value: '1', old_value: null, initial: false },
 				{ value: '1', old_value: '0', initial: false }
 			]);
+		});
+
+		it('should call back with initial, existing values', async function() {
 
 			await evalPage(function() {
 
@@ -53,7 +56,7 @@ describe('DOM Spotting', function() {
 				span.setAttribute('my-attribute', 1);
 			});
 
-			changes = await evalPage(function() {
+			let changes = await evalPage(function() {
 				return window.my_attribute_changes;
 			});
 
@@ -61,6 +64,67 @@ describe('DOM Spotting', function() {
 				{ value: '1', old_value: null, initial: true },
 				{ value: '1', old_value: '0', initial: false }
 			]);
+		});
+
+		it('should check existing elements upon creation', async function() {
+
+			// Elements & listener added in the same tick
+			await evalPage(function() {
+				let span = document.createElement('span');
+				span.textContent = 'span';
+				span.setAttribute('my-other-attribute', 'init');
+				document.body.append(span);
+
+				window.my_attribute_changes = [];
+
+				hawkejs.constructor.onAttribute('my-other-attribute', function(element, value, old_value, initial) {
+
+					my_attribute_changes.push({
+						value     : value,
+						old_value : old_value,
+						initial   : initial
+					});
+				});
+			});
+
+			let changes = await evalPage(function() {
+				return window.my_attribute_changes;
+			});
+
+			assert.strictEqual(changes.length, 1, 'The callback should have only been called once');
+
+			assert.deepStrictEqual(changes, [
+				{ value: 'init', old_value: null, initial: true },
+			]);
+
+			// Elements added first
+			await evalPage(function() {
+				let span = document.createElement('span');
+				span.textContent = 'span';
+				span.setAttribute('my-third-attribute', 'start');
+				document.body.append(span);
+			});
+
+			await __Protoblast.Classes.Pledge.after(20);
+
+			await evalPage(function() {
+				window.my_attribute_changes = [];
+
+				hawkejs.constructor.onAttribute('my-third-attribute', function(element, value, old_value, initial) {
+
+					my_attribute_changes.push({
+						value     : value,
+						old_value : old_value,
+						initial   : initial
+					});
+				});
+			});
+
+			await __Protoblast.Classes.Pledge.after(20);
+
+			changes = await evalPage(function() {
+				return window.my_attribute_changes;
+			});
 		});
 	});
 
