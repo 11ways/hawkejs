eaf9c82272e1d89432a97ec9a7c32bce90222f4f
Jelle De Loecker <jelle@elevenways.be>
2024-04-27 14:19:06 +0200

    ✅ Add prepareVariables benchmark

--- benchmark/05-renderer.js
+++ benchmark/05-renderer.js
@@ -25,16 +25,35 @@ suite('Renderer()', function() {
 		hawkejs.createRenderer();
 	});
 
-	bench('#renderHTML()', function() {
+	bench('renderHTML()', function() {
 		renderer = hawkejs.createRenderer();
 		return renderer.renderHTML('nested_test');
 	});
 
-	bench('#toDry()', function() {
+	bench('toDry()', function() {
 		renderer = hawkejs.createRenderer();
 		variables = main.createTestVariables();
 		renderer.variables = variables;
 		__Protoblast.Bound.JSON.toDryObject(renderer)
+	});
+
+	bench('prepareVariables()', () => {
+
+		let raw_variables = {
+			a_number  : 14,
+			b_string  : 'A string',
+			c_boolean : true,
+			d_array   : [1, 2, 3],
+			e_object  : {
+				a: 1,
+				b: 2,
+				c: 3,
+				rx: /regex/,
+				map: new Map([['a', 1], ['b', 2]]),
+			}
+		};
+
+		let prepared = renderer.prepareVariables(raw_variables);
 	});
 });
 
